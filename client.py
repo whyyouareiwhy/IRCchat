b2b7f1ec8fca8e49874e39b03a57949e57837a25
Yuriy Alexander <yuriy@Mac.local>
2022-05-30 17:35:07 -0700

    Added #general channel and ability to display users

--- client.py
+++ client.py
@@ -8,30 +8,43 @@ import socket
 import threading
 import datetime
 
-CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-CLIENT.connect(('127.0.0.1', 60000))
+HEADER = 1024
 
-nickname = input("Select nickname: ")
-msgTime = datetime.datetime.now()  # Do in thread and within message()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 6060))
+nickname = input("Enter user name: ")
+channels = ['#general']
+# msgTime = datetime.datetime.now()  # Do in thread and within message()
+
 
 def receive():
     while True:
         try:
-            message = CLIENT.recv(1024).decode('ascii')
+            message = client.recv(HEADER).decode('utf-8')
             if 'NICK' == message:
-                CLIENT.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
+            # elif 'QUIT' == message:
+            #     exit(1)
             else:
-                print(message)
+                print(f"{message}")
         except:
             print("Error occurred.")
-            CLIENT.close()
+            client.close()
             break
 
 
 def write():
     while True:
-        message = f'{nickname} [{msgTime.hour}:{msgTime.minute}:{msgTime.second}]: {input("")}'
-        CLIENT.send(message.encode('ascii'))
+        msg_input = f'{nickname}: {input("")}'
+        message = msg_input[len(nickname)+2:]
+        # User can display users in a chat room by typing '/users' and then
+        # the name of the room.
+        if message == "/users":
+            room = input("Enter channel name: ")
+            print(f"client room <{room}>")
+            client.send(room.encode('utf-8'))
+        else:
+            client.send(msg_input.encode('utf-8'))
 
 
 # Start thread, get time, and update time when posting message
--- server.py
+++ server.py
@@ -7,55 +7,83 @@ Server Implementation
 import socket
 import threading
 
+# HOST = socket.gethostbyname(socket.gethostname())
+HOST = '127.0.0.1'  # local host
+PORT = 6060
+HEADER = 1024
 
-HOST = '127.0.0.1'  # localhost
-PORT = 60000
-SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SERVER.bind((HOST, PORT))
-SERVER.listen()
+# Only one server required, it will listen for 1 or more clients
+# and broadcast the messages to everyone
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
 
 clients = []
 nicknames = []
+default_channel = "#general"
+channels = {}
+# List of clients in the '#general' channel created by default
+channels[default_channel] = []
+
 
 def broadcastMsg(msg):
     for client in clients:
         client.send(msg)
 
+def personalMsg(client, msg):
+    client.send(msg)
 
+
+# Called continuously in thread after server-client connection established
 def handleClient(client):
-    while True:
+    connected = True  # Use later for quit command
+    while connected:
         try:
-            msg = client.recv(1024)
-            broadcastMsg(msg)
+            message = client.recv(HEADER).decode('utf-8')
+            if message == "#general":
+                clientsInChannel(client, message)
+            else:
+                broadcastMsg(message.encode('utf-8'))
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nick = nicknames[index]
             nicknames.remove(nick)
-            broadcastMsg(f'{nick} has left!'.encode('ascii'))
+            broadcastMsg(f'{nick} has left!'.encode('utf-8'))
             break
 
 
 def runServer():
+    print('Server running...')
     while True:
-        client, address = SERVER.accept()
+        client, address = server.accept()
 
-        client.send('NICK'.encode('ascii'))
-        nick = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode('utf-8'))
+        nick = client.recv(HEADER).decode('utf-8')
         nicknames.append(nick)
         clients.append(client)
+        channels[default_channel].append(nick)
 
         # Display in server
         print(f'User [{nick}] has connected with: {str(address)}.')
 
         # Display in chatroom
-        broadcastMsg(f'{nick} has joined the channel.'.encode('ascii'))
-        # client.send('Connected to server.'.encode('ascii'))
+        broadcastMsg(f'{nick} has joined the channel.'.encode('utf-8'))
+        client.send('Connected to server.'.encode('utf-8'))
 
         # Handle multiple clients with threading
         thread = threading.Thread(target=handleClient, args=(client,))
         thread.start()
 
-print('Server running...')
+
+def clientsInChannel(client, channel):
+    client.send(f"Users in channel {channel} ".encode('utf-8'))
+    for chan in range(len(channels[channel])):
+        user = channels[default_channel][chan]
+        user += " "
+        client.send(user.encode('utf-8'))
+
+
+# Run server until stopped with ctr-c by user
 runServer()
